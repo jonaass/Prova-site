2297fe9eaf0d01c4b99e736ff360c86ff1fe3ca6
Jonas da cunha madeira <jonascn.2021@gmail.com>
2022-06-24 17:48:37 -0300

    Terminei

--- site/src/pages/consultar/index.js
+++ site/src/pages/consultar/index.js
@@ -1,15 +1,51 @@
-import { useState } from "react";
 import { ConsultarAnime } from "../../api/anime";
+
+import { useEffect, useState } from "react";
+
 
 
 export default function Index() {
     const [animes,SetAnimes] =useState([]);
+
+
+  async function CarregarAnimes() {
+      const resposta= await ConsultarAnime();
+      SetAnimes(resposta);
+  }
+
+  useEffect(() =>{
+      CarregarAnimes();
+  })
     
     
+  return(
+        <main className="pai">
+         <div>
+             <table>
+                     <thead>
+                         <tr>
+                             <th>
+                                 Nome
+                             </th>
+                         </tr>
+                     </thead>
+                     <tbody>
+                         {animes.map(item =>
+                            <tr>
+                                <td>{item.nome}</td>
+                            </tr>
+                            )}
+                     </tbody>
+             </table>
+
+         </div>
+
+    </main>
+  );
 
 
 
-    
 }
+
 
 
